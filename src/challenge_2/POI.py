4de604df2c362647f891551ddfd9190078094761
Noah Parker <nitrogenreaction@gmail.com>
2021-11-16 10:16:21 -0600

    Work on POI Detection and Tracking
    Currently untested. Plan is to use an object tracker from OpenCV to track POIs from frame to frame.
    
    Still need to add brightness based segmentation

--- src/challenge_2/POI.py
+++ src/challenge_2/POI.py
@@ -3,6 +3,7 @@ import cv2
 from dataclasses import dataclass, field
 import functools
 from challenge_2.VisionTests import maskHue
+from cv2.legacy import TrackerMOSSE_create #type: ignore
 
 
 class DefaultVars:
@@ -20,6 +21,24 @@ class DefaultVars:
 #     _, res = cv2.threshold(res, hue*(1-tol), 255, cv2.THRESH_BINARY)
 #     return res
 
+def bbox_union(a,b):
+  x = min(a[0], b[0])
+  y = min(a[1], b[1])
+  w = max(a[0]+a[2], b[0]+b[2]) - x
+  h = max(a[1]+a[3], b[1]+b[3]) - y
+  return (x, y, w, h)
+
+def bbox_intersection(a,b):
+  x = max(a[0], b[0])
+  y = max(a[1], b[1])
+  w = min(a[0]+a[2], b[0]+b[2]) - x
+  h = min(a[1]+a[3], b[1]+b[3]) - y
+  if w<0 or h<0: return (0,0,0,0) # or (0,0,0,0) ?
+  return (x, y, w, h)
+
+def bbox_area(a):
+    _, _, w, h = a
+    return w*h
 
 @dataclass
 class Hue_Detector_Opts:
@@ -28,7 +47,7 @@ class Hue_Detector_Opts:
     opening_size: int = 10
 
     def hueInHSV(self):
-        return (self.target_hue /360)*255
+        return (self.target_hue / 360)*255
 
 
 @functools.cache
@@ -44,25 +63,60 @@ def geoRegister(pix_coords, vehicle_info):
 @dataclass
 class POI_Tracker:
     new_poi_found: bool = field(default=False, init=False)
-    detected_pois: list[Any] = field(default=[], init=False);
+    detected_pois: list[Any] = field(default=[], init=False)
+    poi_trackers: Dict[int, Any] = field(default={}, init=False)
+    highest_assigned_id = field(default=1, init=False)
+    minimum_area: int = 10
     hue_detect_params: Hue_Detector_Opts = Hue_Detector_Opts()
     flow_feature_params: Dict = DefaultVars.optical_flow_feature_params
     flow_params: Dict = DefaultVars.optical_flow_params
     
-    def processFrame(self, frame, vehicle) -> bool:
+    def processFrame(self, vehicle, frame_bgr, frame_hsv=None) -> bool:
+        """Process a frame of HSV video and update POI information/detect new POIs
+        :param frame: Captured frame in BGR format
+        :param vehicle: Dronekit vehicle object
         """
-        Process a frame of HSV video and update POI information/detect new POIs
+        if frame_hsv is None:
+            frame_hsv = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2HSV_FULL)
 
-        :return: true if new POI was found
-        """
-        mask = maskHue(self.hue_detect_params.target_hue, self.hue_detect_params.tolerance, frame)
+
+        mask = maskHue(self.hue_detect_params.hueInHSV(), self.hue_detect_params.tolerance, frame_hsv)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, getOpeningKernel(self.hue_detect_params.opening_size))
 
         num_regions, labels = cv2.connectedComponents(mask)
 
-        #TODO: Image Segmentation?
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = [cont for cont in contours if cv2.contourArea(cont) > self.minimum_area]
+        
+        detected_poi_bboxes = [cv2.boundingRect(x) for x in contours]
+        
+        new_bboxes = {}
+        failed_tracker_ids = [];
+        for id, tracker in self.poi_trackers.items():
+            res, bbox = tracker.update(frame_bgr)
+            if res:
+                new_bboxes[id] = bbox
+            else:
+                failed_tracker_ids.append(id)
+
+        for id in failed_tracker_ids:
+            del self.poi_trackers[id]
+
+        for det_bbox in detected_poi_bboxes:
+            if all([ (bbox_area(bbox_intersection(x, det_bbox)) / bbox_area(det_bbox)) < 0.80 for x in new_bboxes.values()]):
+                new_id = self.highest_assigned_id
+                self.highest_assigned_id += 1
+                new_tracker = TrackerMOSSE_create()
+                new_tracker.init(frame_bgr, det_bbox)
+                self.poi_trackers[new_id] = new_tracker
+                
+            # for track_bbox in new_bboxes.values:
+
+        #TODO: Image Segmentation? - In progress
+        #TODO: POI tracking - In progress
+                
         #TODO: Georegistration
-        #TODO: POI tracking
+            
         return True
 
     def getUnvisitedPOIs(self):
